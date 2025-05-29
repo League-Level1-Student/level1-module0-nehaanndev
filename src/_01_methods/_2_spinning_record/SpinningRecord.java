924f92b6e028baa3ff0cf6af55617bac3a1367b1
neha <neha.ann.dev@gmail.com>
2025-05-28 18:25:40 -0700

    dd

--- src/_01_methods/_2_spinning_record/SpinningRecord.java
+++ src/_01_methods/_2_spinning_record/SpinningRecord.java
@@ -36,54 +36,60 @@ import processing.core.PImage;
  *    the record is spinning.
  */
 public class SpinningRecord extends PApplet {
-    static final int WIDTH = 600;
-    static final int HEIGHT = 600;
-    int spinAmt = 10;
-    
-    Song song = new Song("awesomeTrack.mp3");
-    PImage pictureOfRecord;
-    
-    @Override
-    public void settings() {
-        size(WIDTH, HEIGHT);
-    }
+	static final int WIDTH = 600;
+	static final int HEIGHT = 600;
+	int spinAmt = 0;
 
-    @Override
-    public void setup() {
-    	pictureOfRecord = loadImage("images/record.png");
-    	pictureOfRecord.resize(HEIGHT,WIDTH	);
-    }
+	Song song = new Song("awesomeTrack.mp3");
+	PImage pictureOfRecord;
 
-    @Override
-    public void draw() {
-    image(pictureOfRecord, 0, 0);
-    rotateImage(pictureOfRecord, spinAmt);
-    image(pictureOfRecord, 0, 0);
-    }
+	@Override
+	public void settings() {
+		size(WIDTH, HEIGHT);
+	}
 
-    static public void main(String[] args) {
-        PApplet.main(SpinningRecord.class.getName());
-    }
-    
-    /*********************** DO NOT MODIFY THE CODE BELOW ********************/
+	@Override
+	public void setup() {
+		pictureOfRecord = loadImage("images/record.png");
+		pictureOfRecord.resize(HEIGHT, WIDTH);
+	}
 
-    void rotateImage(PImage image, int amountToRotate) {
-        translate(width / 2, height / 2);
-        rotate(amountToRotate * TWO_PI / 360);
-        translate(-image.width / 2, -image.height / 2);
-    }
+	@Override
+	public void draw() {
+		if (mousePressed == true) {
+			spinAmt += 1000000000;
+			rotateImage(pictureOfRecord, spinAmt);
+			image(pictureOfRecord, 0, 0);
+			song.play();
+		}else {
+			image(pictureOfRecord, 0,0);
+			song.stop();
+		}
+	}
 
-    class Song extends Sound {
-        public Song(String songAddress) {
-            super(songAddress);
-        }
+	static public void main(String[] args) {
+		PApplet.main(SpinningRecord.class.getName());
+	}
 
-        @Override
-        public void play() {
-            if( !super.isPlaying() ) {
-                System.out.println("playing");
-                super.play();
-            }
-        }
-    }
+	/*********************** DO NOT MODIFY THE CODE BELOW ********************/
+
+	void rotateImage(PImage image, int amountToRotate) {
+		translate(width / 2, height / 2);
+		rotate(amountToRotate * TWO_PI / 360);
+		translate(-image.width / 2, -image.height / 2);
+	}
+
+	class Song extends Sound {
+		public Song(String songAddress) {
+			super(songAddress);
+		}
+
+		@Override
+		public void play() {
+			if (!super.isPlaying()) {
+				System.out.println("playing");
+				super.play();
+			}
+		}
+	}
 }
