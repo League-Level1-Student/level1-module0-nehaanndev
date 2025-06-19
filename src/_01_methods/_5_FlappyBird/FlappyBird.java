03faf51bb423d01330a9883ddbc96d1657b47ee6
neha <neha.ann.dev@gmail.com>
2025-06-18 18:25:26 -0700

    JE

--- src/_01_methods/_5_FlappyBird/FlappyBird.java
+++ src/_01_methods/_5_FlappyBird/FlappyBird.java
@@ -5,6 +5,13 @@ import processing.core.PApplet;
 public class FlappyBird extends PApplet {
     static final int WIDTH = 800;
     static final int HEIGHT = 600;
+    int x = 400;
+    int y = 300;
+    int gravity = 1;
+    int birdYVelocity = 0;
+    int X = 0;
+
+
 
     @Override
     public void settings() {
@@ -18,10 +25,21 @@ public class FlappyBird extends PApplet {
 
     @Override
     public void draw() {
-    	background(25, 50, 75);f
-    	fill(red, green, blue);
-    	stroke(red, green, blue);
-    	ellipse(x, y, width, height);
+    	background(25, 50, 75);
+    	fill(10, 200, 50);
+    	rect(X, 500, width, height);
+    	fill(250, 175, 50);
+    	stroke(0, 0, 0);
+    	ellipse(x, y, 50, 50);
+    	x += 10;
+    	X += 10;
+    	y -= birdYVelocity;
+    	birdYVelocity -= gravity;
+
+    }
+    @Override
+    public void mousePressed() {
+    	birdYVelocity = 0;
     }
 
     static public void main(String[] args) {
