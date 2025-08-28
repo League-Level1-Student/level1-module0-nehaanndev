a4a61794528337c27b294e75d74693ab01cd78af
neha <neha.ann.dev@gmail.com>
2025-08-27 18:27:24 -0700

    JSKjd

--- src/_03_gui_with_help/_2_photo_quiz/PhotoQuiz.java
+++ src/_03_gui_with_help/_2_photo_quiz/PhotoQuiz.java
@@ -26,34 +26,45 @@ public class PhotoQuiz {
 		// 1. find an image on the internet, and put its URL in a String
 		// variable (from your browser, right click on the image, and select
 		// “Copy Image Address” )
-
+		String imageURL = "https://abcbirds.org/wp-content/uploads/2019/01/BOTW-Homepage-Thumbnail_Mountain-Bluebird_Double-Brow-Imagery_Shutterstock-768x498.jpg";
 		// 2. create a variable of type "Component" that will hold your image
-
+		
 		// 3. use the "createImage()" method below to initialize your Component
-
+		Component image = createImage(imageURL);
 		// 4. add the image to the quiz window
-
+		quizWindow.add(image);
 		// 5. call the pack() method on the quiz window
-
+		quizWindow.pack();
 		// 6. ask a question that relates to the image
-
-		// 7. print "CORRECT" if the user gave the right answer
-
+		String answer = JOptionPane.showInputDialog("What was the main color of the bird?");
+		// 7. print "CORRECT" if the user gave the right answer 
+		if (answer.equalsIgnoreCase("blue")) {
+			System.out.println("Correct.");
+		}else {
+			System.out.println("Incorrect.");
+		}
 		// 8. print "INCORRECT" if the answer is wrong
 
 		// 9. remove the component from the quiz window (you may not see the
 		// effect of this until step 12)
+		quizWindow.remove(image);
 
 		// 10. find another image and create it (might take more than one line
 		// of code)
-
+		imageURL = "https://wildernessawareness.org/wp-content/uploads/2020/03/zh-istock-000003095919small.jpg";
+		image = createImage(imageURL);
 		// 11. add the second image to the quiz window
-
+		quizWindow.add(image);
 		// 12. pack the quiz window
-
+		quizWindow.pack();
 		// 13. ask another question
-
+		answer = JOptionPane.showInputDialog("What type of bird is this?");
 		// 14+ check answer, say if correct or incorrect, etc.
+		if (answer.equals("eagle")) {
+			System.out.println("Correct.");
+		}else {
+			System.out.println("Incorrect.");
+		}
 
 	}
 
--- src/_03_gui_with_help/_3_drum_kit/DrumKit.java
+++ src/_03_gui_with_help/_3_drum_kit/DrumKit.java
@@ -1,52 +1,62 @@
 package _03_gui_with_help._3_drum_kit;
 
+import java.awt.GridLayout;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.net.URL;
 
 import javax.swing.Icon;
 import javax.swing.ImageIcon;
+import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
+import javax.swing.JPanel;
 
 import game_tools.Sound;
 
 public class DrumKit implements MouseListener {
     static boolean canPlaySounds = true; // Set this to false if your computer cannot play sounds
     JLabel drumLabel;
+    JLabel cymbalLabel;
+    JPanel panel =  new JPanel();
 
     public void run() {
         //  Make a JFrame variable and initialize it using "new JFrame()"
-
+    	JFrame window = new JFrame();
         //  Make the frame visible and
         // set its default close operation to JFrame.EXIT_ON_CLOSE
-
+    	window.setVisible(true);
+    	window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         //  Set the title of the frame
-
+    	window.setTitle("Drum Kit");
         //  Make a JPanel variable and initialize it using "new JPanel().
-
+    	
         //  Add the panel to the frame. (The panel is invisible.)
-
+    	window.add(panel);
         //  Use the image of a drum provided ("snare.jpg") or
         //  download an image of a drum from the Internet and drop it into the
         //  Drum Kit recipe package.
 
         //  Put the name of the drum image file in a String variable.
-
+    	String drumImageSTR = "snare.jpg";
         //  Edit the next line to use your String variable
-        // drumLabel = createLabelImage(drumImageString);
-
+        drumLabel = createLabelImage(drumImageSTR);
         //  Add the label to the panel
-
+        panel.add(drumLabel);
         //  Call the pack() method on the frame.
         // Run your program. Do you see your drum image?
-
+        window.pack();
         // Add this MouseListener to drumLabel
-
+        drumLabel.addMouseListener(this);        
         // *** Write the code in the mouseClicked() method below
-
+        panel.setLayout(new GridLayout());
+    	
+    	String cymbalImageSTR = "cymbal.jpg";
+        cymbalLabel = createLabelImage(cymbalImageSTR);
+        // Print "mouse clicked" to the console. Run your program and watch
         //  Set the layout of the panel to "new GridLayout()"
-
+    	panel.add(cymbalLabel);
+    	cymbalLabel.addMouseListener(this);
         //  Add a cymbal image to make a Drum Kit (one has been provided).
         //  You will need a different sound to go with this image.
         //  Remember to add this MouseListener to it. Run the program.
@@ -55,13 +65,19 @@ public class DrumKit implements MouseListener {
 
     @Override
     public void mouseClicked(MouseEvent e) {
-        // Print "mouse clicked" to the console. Run your program and watch
+  
         // the console to see when this is printed.
 
-        //JLabel labelClicked = (JLabel) e.getSource(); // This line gets the label
+        JLabel labelClicked = (JLabel) e.getSource(); // This line gets the label
                                                         // that the mouse
                                                         // clicked on
 
+        if (labelClicked.equals(drumLabel)) {
+        	playSound("drum.wav");
+        }else {
+        	playSound("cymbal.wav");
+        }
+        
         // You can use the drum sound provided ("drum.wav") or
         // download another drum sound (.wav) and drop it into the Drum Kit package.
         // You can find sounds on freesound.org, and to download from there, you must log in
