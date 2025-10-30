1425a5476fa77ea62452f95b6c78efa1973dc70d
neha <neha.ann.dev@gmail.com>
2025-10-29 18:26:38 -0700

    Need to add the runner for Jack in box

--- src/_04_gui_from_scratch/_1_fortune_cookie/FortuneCookie.java
+++ src/_04_gui_from_scratch/_1_fortune_cookie/FortuneCookie.java
@@ -3,9 +3,11 @@ package _04_gui_from_scratch._1_fortune_cookie;
 import java.awt.Dimension;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.Random;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
 public class FortuneCookie implements ActionListener {
@@ -24,7 +26,25 @@ public class FortuneCookie implements ActionListener {
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		// TODO Auto-generated method stub
-		
+		JOptionPane.showMessageDialog(null, "Woohoo");
+		int rand = new Random().nextInt(5);
+		if (rand == 0) {
+			JOptionPane.showMessageDialog(null, "Follow you dreams.");
+		}
+		else if (rand == 1) {
+			JOptionPane.showMessageDialog(null, "You will reach success through hardwork.");
+		}
+		else if (rand == 2) {
+			JOptionPane.showMessageDialog(null, "It is okay to take time for yourself.");
+		}
+		else if (rand == 3) {
+			JOptionPane.showMessageDialog(null, "Surround yourself with those who care.");
+		}
+		else if (rand == 4) {
+			JOptionPane.showMessageDialog(null, "Sleep is important, just like you.");
+		} else {
+			JOptionPane.showMessageDialog(null, "A error occured.");
+		}
 	}
 }
 
---
+++ src/_04_gui_from_scratch/_2_jack_in_the_box/JackInBox.java
@@ -0,0 +1,79 @@
+package _04_gui_from_scratch._2_jack_in_the_box;
+
+import java.applet.AudioClip;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.net.URL;
+
+import javax.swing.Icon;
+import javax.swing.ImageIcon;
+import javax.swing.JApplet;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+public class JackInBox implements ActionListener {
+	int count = 0;
+
+	public void setup() {
+		JFrame frame = new JFrame();
+		frame.setSize(100, 100);
+		frame.setVisible(true);
+
+		JButton surpriseButton = new JButton();
+		surpriseButton.setText("Click Me for a Surprise!");
+
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		count += 1;
+		System.out.print("The button has been clicked " + count + " times.");
+		if (count == 5) {
+			showPicture("jackInTheBox.png");
+			createLabelImage("jackInTheBox.png");
+			playSound("homer-woohoo.wav");
+
+		}
+	}
+
+	private void showPicture(String fileName) {
+		try {
+			JLabel imageLabel = createLabelImage(fileName);
+			JFrame frame = new JFrame();
+			frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+			frame.add(imageLabel);
+			frame.setVisible(true);
+			frame.pack();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	private JLabel createLabelImage(String fileName) {
+		try {
+			URL imageURL = getClass().getResource(fileName);
+			if (imageURL == null) {
+				System.err.println("Could not find image " + fileName);
+				return new JLabel();
+			} else {
+				Icon icon = new ImageIcon(imageURL);
+				JLabel imageLabel = new JLabel(icon);
+				return imageLabel;
+			}
+		} catch (Exception e) {
+			System.err.println("Could not find image " + fileName);
+			return new JLabel();
+		}
+	}
+
+	private void playSound(String soundFile) {
+		try {
+			AudioClip sound = JApplet.newAudioClip(getClass().getResource(soundFile));
+			sound.play();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
